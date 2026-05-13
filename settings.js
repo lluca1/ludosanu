23172a84b18a9f1241979169bf47b16fa5b41c99
lluca1 <ludosanuluca@gmail.com>
2026-05-13 22:30:45 +0300

    infinity columnify website -restyle page with my transience project animation as the background -archive the old page -recolor cat.svg -add gitignore

---
+++ settings.js
@@ -0,0 +1,42 @@
+export const settings = 
+
+
+
+{
+  "quality": 1,
+  "cameraX": 5.3,
+  "cameraY": -1.1,
+  "cameraZ": 6.7,
+  "orbitEnabled": 0,
+  "orbitRadius": 9,
+  "orbitYaw": 0,
+  "orbitPitch": 8,
+  "orbitSpeed": 0.15,
+  "tiltX": -35,
+  "tiltZ": -62,
+  "targetX": 0,
+  "targetY": 0,
+  "targetZ": 0,
+  "fov": 50,
+  "base": 1.4,
+  "amplitude": 0.45,
+  "wavePeriod": 4,
+  "cyclesPerChunk": 3,
+  "numChunks": 3,
+  "pointsPerChunk": 30000,
+  "pointSize": 0.021,
+  "scrollSpeed": 0.35,
+  "loopDuration": 60,
+  "deformX": 0,
+  "deformY": 0,
+  "deformZ": 0,
+  "deformRadius": 0.5,
+  "swirlStrength": 30,
+  "radialStrength": 0.9,
+  "chaosStrength": 0.35,
+  "chaosFreq": 3
+}
+
+
+
+;
